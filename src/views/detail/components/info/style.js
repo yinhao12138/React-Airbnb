aa2fe1c02d124da438d5e2b6954465145b8baa95
yinhao12138 <2531134419@qq.com>
2023-12-04 18:15:36 +0800

    test

--- src/views/detail/components/info/style.js
+++ src/views/detail/components/info/style.js
@@ -2,6 +2,6 @@ import { styled } from "styled-components";
 
 export const InfoWrapper = styled.div`
   width: 100px;
-  height: 3000px;
-  background-color: red;
+  height: 4000px;
+  background-color: green;
 `;
